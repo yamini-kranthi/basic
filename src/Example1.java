60552e77b7e9907f57f008f7b2a94db4a398ff54
yamini-kranthi <102395783+yamini-kranthi@users.noreply.github.com>
2022-03-29 14:01:24 -0700

    Update Example1.java

--- src/Example1.java
+++ src/Example1.java
@@ -4,6 +4,9 @@ public class Example1 {
 	public static void main(String[] args) {
        System.out.println("hello world");
 		  System.out.println("hello my love");
+				  System.out.println("******");
+
+		
 	}
 
 }
